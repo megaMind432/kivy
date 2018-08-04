935f833b6bb3e3405d4833e0e774c4106c916a0c
mehedi_432 <36066846+megaMind432@users.noreply.github.com>
2018-08-05 01:59:01 +0600

    upload label
    A simple example of Label.

---
+++ label.py
@@ -0,0 +1,10 @@
+from kivy.app import App
+from kivy.uix.label import Label
+
+class SimpleLabel(App):
+    def build(self):
+        return Label(text = "This is just a Label")
+
+
+if __name__ == "__main__":
+    SimpleLabel().run()
